8fdce330e0eee2b08a4f6fe03e5cbb2e559fc435
Aryan Tayal <bookworms@ariansh.com>
2023-03-17 11:18:35 +0530

    finish project

---
+++ script.js
@@ -0,0 +1,20 @@
+// https://api.adviceslip.com/advice
+
+const adviceText = document.querySelector("#adviceText");
+const adviceNumber = document.querySelector("#adviceNumber > span");
+const button = document.querySelector("#dice");
+
+const writeAdvice = async () => {
+  try {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        adviceNumber.innerText = data.slip.id;
+        adviceText.innerText = data.slip.advice;
+      });
+  } catch (e) {
+    console.log("ERROR!", e);
+  }
+};
+dice.addEventListener("click", writeAdvice);
+window.addEventListener("load", writeAdvice);
